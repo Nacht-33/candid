e2948ed74a59ff9d0b039fbf99ae9623a4fcf665
Nacht-33 <singhadhiman652@gmail.com>
2023-02-21 11:36:06 +0530

    Adding SignUp Page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+import LoginForm from "./Components/Forms/login-forms";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
